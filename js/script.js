40ef65138fed333e334c24bfef6f95c44875f692
name <email@example.ru>
2022-10-01 23:02:11 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const btnPrev = document.querySelector(".slider__button--prev");
+const btnNext = document.querySelector(".slider__button--next");
+const slider = document.querySelector(".slider__list");
+/* const cards = document.querySelectorAll(".slider__item"); */
+
+let left = 0;
+let imgWidth = 1180;
+
+const slideClick = (side) => {
+  if (side == "left") {
+    left -= imgWidth;
+  } else {
+    left += imgWidth;
+  }
+
+  if (left < -3540) {
+    left = 0;
+  }
+
+  if (left > 0) {
+    left = -3540;
+  }
+
+  slider.style.left = left + "px";
+};
+
+btnPrev.addEventListener("click", () => {
+  slideClick("right");
+});
+
+btnNext.addEventListener("click", () => {
+  slideClick("left");
+});
